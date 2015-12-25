d57d42d9b1a2ea4d675a663bae8f4af70e42aa53
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-12-24 20:14:28 -0600

    removed commented-out view func

--- wikivision/view.py
+++ wikivision/view.py
@@ -14,11 +14,3 @@ def index():
     else:
         tree_data = None
     return render_template('index.html', tree_data=tree_data)
-
-
-# @app.route('/data/<article_slug>')
-# def data(article_slug):
-#     revisions = get_article_revisions(article_slug)
-#     # tree_data = tree_format(revisions)
-#     tree_data = {'nodes': revisions.to_dict('records')}
-#     return jsonify(**tree_data)
