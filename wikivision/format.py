638afb5e9bb4bb688ca4b3f8e9fc3b43b9a3a0c7
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-12-29 23:25:08 -0600

    use google style guide

--- wikivision/format.py
+++ wikivision/format.py
@@ -2,7 +2,14 @@ import logging
 
 
 def tree_format(revisions):
-    """ Convert a complete revision history to a tree format. """
+    """Convert a complete revision history to a tree format.
+
+    Args:
+        revisions: A pandas.DataFrame of revisions to an article.
+    Returns:
+        A root node (a dict) with children nodes containg all versions of the
+        article.
+    """
     revisions = revisions.copy()
     revisions = drop_repeats(revisions)
     revisions = label_version(revisions)
@@ -18,6 +25,15 @@ def tree_format(revisions):
 
 
 def drop_repeats(revisions):
+    """Drop rows containing repeated wikitext.
+
+    Repeats are detected by comparing subsequent versions of the article text.
+
+    Args:
+        revisions: A pandas.DataFrame of revisions to an article.
+    Returns:
+        A copy of revisions with repeated rows removed.
+    """
     revisions = revisions.copy()
     is_repeat = revisions.wikitext[1:] == revisions.wikitext[:-1]
     revisions['is_repeat'] = is_repeat
@@ -26,8 +42,31 @@ def drop_repeats(revisions):
     return revisions.ix[~revisions.is_repeat].drop('is_repeat', axis=1)
 
 
+def drop_reversions(revisions):
+    """Drop revisions that revert the article to a previous state.
+
+    Reversions are repeats with intervening revisions.
+
+    Args:
+        revisions: A pandas.DataFrame of revisions to an article.
+    Returns:
+        A copy of revisions without reversions.
+    """
+    is_reversion = (revisions.wikitext_version <
+                    revisions.wikitext_parent_version)
+    logging.info('dropping {} reversions'.format(is_reversion.sum()))
+    return revisions.ix[~is_reversion]
+
+
 def label_version(revisions):
-    """ Label the unique versions of an article. """
+    """Label the unique versions of an article.
+
+    Args:
+        revisions: A pandas.DataFrame of revisions to an article.
+    Returns:
+        A copy of revisions with new columns that label the current version
+        and the version of the parent.
+    """
     revisions = _label_wikitext_version(revisions)
     revisions = _label_wikitext_parent_version(revisions)
     return revisions
@@ -54,10 +93,3 @@ def _label_wikitext_parent_version(revisions):
             wikitext_parent_ids.append(parent_wikitext_id)
     revisions['wikitext_parent_version'] = wikitext_parent_ids
     return revisions
-
-
-def drop_reversions(revisions):
-    is_reversion = (revisions.wikitext_version <
-                    revisions.wikitext_parent_version)
-    logging.info('dropping {} reversions'.format(is_reversion.sum()))
-    return revisions.ix[~is_reversion]
