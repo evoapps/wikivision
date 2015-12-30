016d83b18787138d43706826e2bfeb1995879eca
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-12-30 12:42:37 -0600

    use google style guide for js

--- wikivision/static/revisiontree.js
+++ wikivision/static/revisiontree.js
@@ -23,40 +23,39 @@ function revisionTree(root) {
 
   // Compute the new tree layout.
   var nodes = tree.nodes(root).reverse(),
-	    links = tree.links(nodes);
+      links = tree.links(nodes);
 
   // Normalize for fixed-depth.
   nodes.forEach(function(d) { d.y = d.depth * 100; });
 
-  // Declare the nodes…
+  // Declare the nodes
   var node = svg.selectAll("g.node")
-	  .data(nodes, function(d) { return d.id || (d.id = ++i); });
+    .data(nodes, function(d) { return d.id || (d.id = ++i); });
 
   // Enter the nodes.
   var nodeEnter = node.enter().append("g")
-	  .attr("class", "node")
-	  .attr("transform", function(d) {
-		  return "translate(" + d.x + "," + d.y + ")"; });
+    .attr("class", "node")
+    .attr("transform", function(d) {
+      return "translate(" + d.x + "," + d.y + ")"; });
 
   nodeEnter.append("circle")
-	  .attr("r", 10)
-	  .style("fill", "#fff");
+    .attr("r", 10)
+    .style("fill", "#fff");
 
   nodeEnter.append("text")
-	  .attr("y", function(d) {
-		  return d.children || d._children ? -18 : 18; })
-	  .attr("dy", ".35em")
-	  .attr("text-anchor", "middle")
-	  .text(function(d) { return d.wikitext; })
-	  .style("fill-opacity", 1);
+    .attr("y", function(d) {
+      return d.children || d._children ? -18 : 18; })
+    .attr("dy", ".35em")
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d.wikitext; })
+    .style("fill-opacity", 1);
 
-  // Declare the links…
+  // Declare the links
   var link = svg.selectAll("path.link")
-	  .data(links, function(d) { return d.target.id; });
+    .data(links, function(d) { return d.target.id; });
 
   // Enter the links.
   link.enter().insert("path", "g")
-	  .attr("class", "link")
-	  .attr("d", diagonal);
-
+    .attr("class", "link")
+    .attr("d", diagonal);
 }
